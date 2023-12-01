0ccc1b4a05500b81164836853852cfc3a9245c4a
Amos <hzh990094740@outlook.com>
2023-12-01 11:21:25 +0800

    新增单元测试项目

---
+++ framework/test/Amos.Abp.TestBase/AmosAbpTestDataSeedContributor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+
+namespace Amos.Abp.TestBase
+{
+    public class AmosAbpTestDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        public Task SeedAsync(DataSeedContext context)
+        {
+            /* Seed additional test data... */
+
+            return Task.CompletedTask;
+        }
+    }
+}
