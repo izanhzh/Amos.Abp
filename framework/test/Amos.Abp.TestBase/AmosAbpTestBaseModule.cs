eb2bc2a901b5aad75aa6343932fbca9789d8b516
huangzhenhua <it007@gzbaoshen.com>
2023-12-01 11:21:25 +0800

    新增单元测试项目

---
+++ framework/test/Amos.Abp.TestBase/AmosAbpTestBaseModule.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.Data;
+using Volo.Abp.Modularity;
+using Volo.Abp.Threading;
+
+namespace Amos.Abp.TestBase
+{
+    [DependsOn(typeof(AbpAutofacModule))]
+    [DependsOn(typeof(AbpTestBaseModule))]
+    public class AmosAbpTestBaseModule : AbpModule
+    {
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            SeedTestData(context);
+        }
+
+        private static void SeedTestData(ApplicationInitializationContext context)
+        {
+            AsyncHelper.RunSync(async () =>
+            {
+                using (var scope = context.ServiceProvider.CreateScope())
+                {
+                    await scope.ServiceProvider
+                        .GetRequiredService<IDataSeeder>()
+                        .SeedAsync();
+                }
+            });
+        }
+    }
+}
